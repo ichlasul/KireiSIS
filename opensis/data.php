abfd0506b5366e1657d5087af67bae1484fcfa5b
Ichlasul Amal <13511075@std.stei.itb.ac.id>
2014-02-24 20:42:08 +0700

    Cleanup

--- opensis/data.php
+++
@@ -1,8 +0,0 @@
-<?php 
-$DatabaseType = 'mysql'; 
-$DatabaseServer = 'localhost'; 
-$DatabaseUsername = 'root'; 
-$DatabasePassword = ''; 
-$DatabaseName = 'opensis2'; 
-$DatabasePort = '3306'; 
-?>
